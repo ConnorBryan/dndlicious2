fc3a7497184ac2971ba046f8d779961cd4d32ad4
Connor Bryan <cchromium@gmail.com>
2019-05-01 19:17:15 -0500

    https

--- server/index.js
+++ server/index.js
@@ -1,16 +1,13 @@
-// const fs = require("fs");
-const http = require("http");
-// const https = require("https");
+const fs = require("fs");
+const https = require("https");
 const WebSocket = require("ws");
 
-// if (process.env.NODE_ENV === "production") {
-// const cert = fs.readFileSync("server/server.cert", "utf8");
-// const key = fs.readFileSync("server/server.key", "utf8");
-// const server = https.createServer({
-//   cert,
-//   key
-// });
-const server = http.createServer();
+const cert = fs.readFileSync("server/server.cert", "utf8");
+const key = fs.readFileSync("server/server.key", "utf8");
+const server = https.createServer({
+  cert,
+  key
+});
 const wsServer = new WebSocket.Server({ server });
 const connectedSockets = [];
 
@@ -25,24 +22,3 @@ wsServer.on("connection", socket => {
     connectedSockets.splice(connectedSockets.indexOf(socket), 1);
   });
 });
-
-server.listen(9000);
-// } else {
-//   const server = http.createServer();
-//   const wsServer = new WebSocket.Server({ server });
-//   const connectedSockets = [];
-
-//   wsServer.on("connection", socket => {
-//     connectedSockets.push(socket);
-
-//     socket.on("message", message => {
-//       connectedSockets.forEach(socket => socket.send(message));
-//     });
-
-//     socket.on("close", () => {
-//       connectedSockets.splice(connectedSockets.indexOf(socket), 1);
-//     });
-//   });
-
-//   server.listen(9000);
-// }
